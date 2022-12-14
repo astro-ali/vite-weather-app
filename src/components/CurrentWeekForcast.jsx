a2b4041d2eacf91c3193f48ba51fe5fc45603a2c
Ali Hameed <ali.seemorad1177@gmail.com>
2022-12-14 20:19:28 +0300

    fix

--- src/components/CurrentWeekForcast.jsx
+++ src/components/CurrentWeekForcast.jsx
@@ -8,7 +8,7 @@ const CurrentWeekForcast = ({ forcast }) => {
     <>
       <div className="mb-8 mt-12 md:mt-0">
         <h1 className="text-xl px-4 md:px-0 md:text-3xl font-semibold text-gray-800">
-          Weather Forcast for next 8 Hours ...
+          Weather Forcast
         </h1>
       </div>
       <div className="flex flex-wrap md:flex-nowrap gap-7 justify-around mb-8 md:mb-20">
